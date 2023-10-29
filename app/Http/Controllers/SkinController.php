3535c10917a8d77f118a9b32979917349a3cd354
Eberth <itec_19@hotmail.com>
2023-10-29 11:51:58 +0100

    Skin Model created

---
+++ app/Http/Controllers/SkinController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Skin;
+use Illuminate\Http\Request;
+
+class SkinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Skin $skin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Skin $skin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Skin $skin)
+    {
+        //
+    }
+}
